34345e064885404b489e1fb3e92da4ee94b9c344
niroshan123 <niroshanratnayake07@gmail.com>
2020-06-25 22:21:23 +0530

    promtion added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
 
 
 @NgModule({
@@ -41,7 +42,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatCardModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService,
+  PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
